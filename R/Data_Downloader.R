1d317faef27b3c773c5e9cf29375b95be2618656
Zreka <feisalomarahmad@hotmail.de>
2021-07-08 14:59:47 +0200

    Data_Downloader erstellt braucht noch bissl arbeit
    Man muss noch mit dem string aus direction mit einem namen.csv anhängen

---
+++ R/Data_Downloader.R
@@ -0,0 +1,3 @@
+url <- "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
+destfile <- "C:\\Users\\Admin\\Documents\\COVID-Project\\R\\download.csv"
+download.file(url, destfile)
