9c05e789c411f1e50ecc6e0e499650bc51bc42c3
Zreka <feisalomarahmad@hotmail.de>
2021-07-20 16:50:54 +0200

    Its a function now

--- R/Data_Downloader.R
+++ R/Data_Downloader.R
@@ -1,5 +1,6 @@
-url <- "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
-direction <- dirname(rstudioapi::getSourceEditorContext()$path)
-destfile <- paste(direction, "/data.csv", sep="")
-destfile
-download.file(url, destfile)
+data_downloader <- function(){
+  url <- "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
+  direction <- dirname(rstudioapi::getSourceEditorContext()$path)
+  destfile <- paste(direction, "/data.csv", sep="")
+  #destfile
+  download.file(url, destfile)}
