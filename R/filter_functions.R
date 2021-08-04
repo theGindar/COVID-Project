098cdc650a33ddc97b0c4a1926e904d9208f99b6
theGindar <patrick.guenther30@gmail.com>
2021-08-04 14:52:49 +0200

    add get_incidence_per_district

--- R/filter_functions.R
+++ R/filter_functions.R
@@ -1,8 +1,12 @@
+#install.packages("RcppRoll")
 library(tidyverse)
 library(dplyr)
 library(stringr)
-cov_data <- read.csv("data.csv")
+library(lubridate)
+library(RcppRoll)
 
+cov_data <- read.csv("data.csv")
+source("R/population.R")
 
 get_deaths_per_federal_states <- function(data, age_group_start = NA, age_group_end = NA, federal_state = NA, date_start = NA, date_end = NA) {
   federal_state_names = c("Schleswig-Holstein",
@@ -537,18 +541,56 @@ remove_unclean_data <- function(cov_data){
 
 cov_data <- read.csv("R/RKI_COVID19.csv")
 
-# from infections function
-cov_data %>%
-  filter(NeuerFall %in% c(0,1)) %>%
-  group_by(Refdatum, IdLandkreis, Landkreis, Altersgruppe) %>%
-  summarize(Infections = sum(AnzahlFall)) -> infect_data
-
-# don't care about age groups
-infect_data %>%
-  group_by(Refdatum, IdLandkreis, Landkreis) %>%
-  summarize(Infections = sum(Infections)) -> infect_data
+get_incidence_per_district <- function(data, incidence_days = 7) {
+  # from infections function
+  data %>%
+    filter(NeuerFall %in% c(0,1)) %>%
+    group_by(Meldedatum, IdLandkreis, Landkreis, Altersgruppe) %>%
+    summarize(Infections = sum(AnzahlFall)) -> infect_data
 
 
 
-infect_data
-cov_data
+  # don't care about age groups
+  infect_data %>%
+    group_by(Meldedatum, IdLandkreis, Landkreis) %>%
+    summarize(Infections = sum(Infections)) -> infect_data
+
+  # add population data
+  population_data <- read.csv("R/population_data/population_data_df.csv")
+  infect_data <- left_join(infect_data, population_data, by=c("IdLandkreis" = "LandkreisId"))
+
+  infect_data <- ungroup(infect_data)
+  lk_ids <- distinct(infect_data, IdLandkreis)$IdLandkreis
+  lk_ids
+
+  result_df <- data.frame()
+  message("Calculating Incidences")
+  pb = txtProgressBar(min = 0, max = length(lk_ids), initial = 0)
+  ind <- 1
+  for (lk_id in lk_ids) {
+    infect_data %>%
+      filter(IdLandkreis == lk_id) -> infect_data_district
+
+    infect_data_district %>%
+      arrange(Meldedatum) %>%
+      ungroup %>%
+      mutate(Combined_Infections = roll_sum(Infections,
+                                            incidence_days,
+                                            align = "right",
+                                            fill = NA)) -> infect_data_district
+
+    result_df <- rbind(infect_data_district, result_df)
+    setTxtProgressBar(pb,ind)
+    ind <- ind+1
+  }
+
+  result_df %>%
+    mutate(Inzidenz = Combined_Infections/Population*100000) -> result_df
+
+  result_df %>%
+    select(-c("Population", "Infections", "Combined_Infections")) -> result_df
+
+  return(result_df)
+}
+
+
