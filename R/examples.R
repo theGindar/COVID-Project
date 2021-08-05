87c9a6fe2aaaf4d2c15838549617937d03adb676
theGindar <patrick.guenther30@gmail.com>
2021-08-05 11:47:03 +0200

    add example

---
+++ R/examples.R
@@ -0,0 +1,11 @@
+source("R/filter_functions.R")
+source("R/plot_functions.R")
+
+cov_data <- read.csv("data.csv")
+
+# --- plot incidence matrix ---
+incidences_df <- get_incidence_per_district(cov_data, 7)
+incidence_correlation_pairs <- get_correlation_for_incidence_pairs(incidences_df)
+
+plot_incidence_correlations_matrix(incidence_correlation_pairs,
+                                   districts = c("SK Bochum", "SK Dortmund", "LK Esslingen"))
